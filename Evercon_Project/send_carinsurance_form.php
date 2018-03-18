f6fd5b590ac87f0edf1ecd51e79654175bf1ad2c
André Antonio Faruolo Filho <andre_antonio1900@hotmail.com>
2018-03-18 20:35:24 -0300

    First Commit

---
+++ Evercon_Project/send_carinsurance_form.php
@@ -0,0 +1,62 @@
+
+<html>
+
+<?php
+
+$vigencia_de = $_POST['vigencia_de'];
+$vigencia_a = $_POST['vigencia_a'];
+$seguro_novo = $_POST['seguro_novo'];
+$renovacao = $_POST['renovacao'];
+$cia_atual = $_POST['cia_atual'];
+$bonus = $_POST['bonus'];
+$cpf = $_POST['cpf'];
+$cnh = $_POST['cnh'];
+$estado_civil = $_POST['estado_civil'];
+$data_primeira_habilitacao = $_POST['data_primeira_habilitacao'];
+$endereco = $_POST['endereco'];
+$complemento = $_POST['complemento'];
+$bairro = $_POST['bairro'];
+$cidade = $_POST['cidade'];
+$estado = $_POST['estado'];
+$cep = $_POST['cep'];
+$profissao = $_POST['profissao'];
+$telefone = $_POST['telefone'];
+$email = $_POST['email'];
+$relacao_condutor_principal_segurado = $_POST['relacao_condutor_principal_segurado'];
+$idade_condutor = $_POST['idade_condutor'];
+$estado_civil_condutor = $_POST['estado_civil_condutor'];
+$cpf_condutor = $_POST['cpf_condutor'];
+$cnh_condutor = $_POST['cnh_condutor'];
+$data_habilitacao_condutor = $_POST['data_habilitacao_condutor'];
+$profissao_condutor = $_POST['profissao_condutor'];
+$condutores_casa_dirigem = $_POST['condutores_casa_dirigem'];
+$condutores_casa_nao_dirigem = $_POST['condutores_casa_nao_dirigem'];
+$distancia_trabalho = $_POST['distancia_trabalho'];
+$quilometragem_mensal = $_POST['quilometragem_mensal'];
+$garagem_fechada = $_POST['garagem_fechada'];
+$possui_garagem_casa = $_POST['possui_garagem_casa'];
+$possui_garagem_trabalho = $_POST['possui_garagem_trabalho'];
+$possui_garagem_escola = $_POST['possui_garagem_escola'];
+$tipo_residencia = $_POST['tipo_residencia'];
+$possui_portao = $_POST['possui_portao'];
+$marca = $_POST['marca'];
+$modelo = $_POST['modelo'];
+$placa = $_POST['placa'];
+$portas = $_POST['portas'];
+$chassi = $_POST['chassi'];
+$renavam = $_POST['renavam'];
+$zero_km = $_POST['zero_km'];
+$cambio = $_POST['cambio'];
+$cor = $_POST['cor'];
+$combustivel = $_POST['combustivel'];
+$possui_antifurto = $_POST['possui_antifurto'];
+$tipo_antifurto = $_POST['tipo_antifurto'];
+
+
+
+        
+        ?>
+
+
+
+</html>
